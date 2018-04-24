7b62e991d9f6b148fb9d5a6599f339507c4cf0bd
Sasha Bajzek <dabridginator@gmail.com>
2018-04-24 07:00:03 -0700

    Add copy

---
+++ src/components/AboutMe/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+const AboutMe = () => (
+  <section>
+    <h2>Welcome</h2>
+    <section>
+      <p>
+        Name: Christopher Ragsdale
+      </p>
+      <p>
+        Occupation: Mechanical & Aerospace Engineer
+      </p>
+      <p>
+        Employer: Spectrum Technology Group, Inc.
+      </p>
+      <p>
+        Interests: Space, Space Systems Design, Space Colonization, Space Commercialization,
+        Advanced Technology & Applications, the Future, the Past, Science Fiction, Science Fact (& Theories), Engineering Methodology, Materials Science, Entrepreneurship.
+      </p>
+      <p>
+        Hobbies: Learning, Electronics Design & Fabrication, Attending Space Events, Computer Building & Gaming.
+      </p>
+    </section>
+    <section>
+      <p>
+        I'm clearly very interested in space technology and, well, technology in general. I enjoy exploring and discovering interesting ways different technologies and modes of thinking can be combined to create new and wonderous things. Those combinations tend to be most prevalent in the most extreme environments (space being a good example).
+      </p>
+      <p>
+        While my passion is outer space, the environment where I currently get to put my designs to the test isn't beyond the clouds, but rather beneath the waves.
+      </p>
+    </section>
+    <section>
+      <p>
+        I currently live in San Francisco, California with my amazing wife Sasha. I have had the distinct pleasure of living in quite a few parts of the United States throughout my life (most of them during my childhood).
+      </p>
+      <p>
+        Some of the places I have called home over the years are:
+      </p>
+      <ul>
+        <li>Mount Airy, Maryland</li>
+        <li>Chicago, Illinois</li>
+        <li>Honolulu, Hawaii</li>
+        <li>Landover, Maryland</li>
+        <li>Groton, Connecticut</li>
+        <li>New Port News, Virginia</li>
+      </ul>
+    </section>
+  </section>
+);
+
+export default AboutMe;
---
+++ src/variables.js
@@ -0,0 +1,9 @@
+// Typography
+
+export const typography = {
+  mainFont: "'Dosis', sans-serif",
+  extraLightWeight: 200,
+  regularWeight: 400,
+  semiBoldWeight: 600
+  
+};
