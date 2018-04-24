f21c457f18997197b7a0a8fd13fe6715c249e110
Sasha Bajzek <dabridginator@gmail.com>
2018-04-24 07:41:22 -0700

    Set up SASS

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../Header';
 import AboutMe from '../AboutMe';
-import './App.css';
+import './App.scss';
 
 class App extends React.Component {
   render() {
